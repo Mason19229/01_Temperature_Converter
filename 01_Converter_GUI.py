eeb42711806fd4cbbebb3060641d9187de914758
Mason19229 <132321666+Mason19229@users.noreply.github.com>
2023-05-05 13:28:45 +1200

    Update 01_Converter_GUI.py

--- 01_Converter_GUI.py
+++ 01_Converter_GUI.py
@@ -7,10 +7,11 @@ class Converter:
     def __init__(self):
         
         #formatting variables
-        background_color = "light blue"
+        background_color = "light blue"                                                        #GO THROUGH PREV VIDEO TO GET COMPLETE CORRECT CODE
 
         #Converter Frame
-        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
         self.converter_frame.grid()
 
         #Temperature Converter Heading (row 0)
@@ -22,16 +23,48 @@ class Converter:
 
         #User instructions (row 1)
         self.instructions_label = Label(self.converter_frame, text="Type in temperature below and then push one of the buttons",
-                                        font=("Arial", "12", "italic"),
+                                        font=("Arial", "10", "italic"), wrap=250,
+                                        justify=LEFT,
                                         background=background_color)
+        self.instructions_label.grid(row=1)
 
         #Temperature entry box (row 2)
+        self.temp_entry = Entry(self.converter_frame, width=20,
+                                font=("Arial 14 bold"))
+        self.temp_entry.grid(row=2)
 
-        #Conversion buttons frame (row 3)
+        #Conversion buttons frame (row 3), orchid3 | khaki1
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3)
+
+            #Convert to Centigrade button
+        self.centi_button = Button(self.conversion_buttons_frame, text="to Centigrade",
+                                   font="Arial 10 bold", bg="khaki1", 
+                                   padx=10, pady=10)
+        self.centi_button.grid(row=0, column=0)
+
+            #Convert to Fahrenheit button
+        self.fahren_button = Button(self.conversion_buttons_frame, text="to Fahrenheit",
+                                    font="Arial 10 bold", bg="Orchid1", 
+                                    padx=10, pady=10)
+        self.fahren_button.grid(row=0, column=1)
 
         #Answer label (row 4)
+        self.answer_label = Label(self.converter_frame, text="Conversion will appear here...",
+                                        justify=CENTER,
+                                        background=background_color)
+        self.instructions_label.grid(row=1)
 
         #History / Help button frame (row 5)
+            #Calculation history button
+        self.history_button = Button(self.converter_frame, text="Calculation History", 
+                                  padx=10, pady=10)
+        self.history_button.grid(row=5, column=0)
+
+            #Help button
+        self.help_button = Button(self.converter_frame, text="Help", 
+                                  padx=10, pady=10)
+        self.help_button.grid(row=5, column=1)
 
 # main routine
 if __name__ == "__main__":

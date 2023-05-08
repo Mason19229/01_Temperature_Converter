2a426b9527444f5f2545c32ab618c9e24e319031
Mason19229 <132321666+Mason19229@users.noreply.github.com>
2023-05-09 09:14:01 +1200

    Created 2 seperate files for temperature conversion

---
+++ Temp_Converter_Celsius.py
@@ -0,0 +1,10 @@
+#Temp_Converter_Celsius.py
+#Converting temperature from fahrenheit to celsius
+#Mason Smythe
+
+
+fahrenheit = int(input("Please enter the temperature in degrees Fahrenheit: "))
+
+celsius = (fahrenheit-32)*5/9
+
+print(f"{celsius}°C")
---
+++ Temp_Converter_Fahrenheit.py
@@ -0,0 +1,11 @@
+#Temp_Converter_Fahrenheit.py
+#Converting temperature from celsius to fahrenheit
+#Mason Smythe
+
+
+celsius = int(input("Please enter the temperature in degrees Celsius: "))
+
+fahrenheit = (celsius*9/5)+32
+
+print(f"{fahrenheit}°F")
+

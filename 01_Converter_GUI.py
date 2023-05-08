f52c9cbdf4f73a79c83971157125b7a1b527b0e1
Mason19229 <132321666+Mason19229@users.noreply.github.com>
2023-05-09 09:04:48 +1200

    Update 01_Converter_GUI.py

--- 01_Converter_GUI.py
+++ 01_Converter_GUI.py
@@ -31,11 +31,11 @@ class Converter:
         #Temperature entry box (row 2)
         self.temp_entry = Entry(self.converter_frame, width=20,
                                 font=("Arial 14 bold"))
-        self.temp_entry.grid(row=2)
+        self.temp_entry.grid(row=2, pady=10)
 
         #Conversion buttons frame (row 3), orchid3 | khaki1
         self.conversion_buttons_frame = Frame(self.converter_frame)
-        self.conversion_buttons_frame.grid(row=3)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
 
             #Convert to Centigrade button
         self.centi_button = Button(self.conversion_buttons_frame, text="to Centigrade",
@@ -56,15 +56,18 @@ class Converter:
         self.instructions_label.grid(row=1)
 
         #History / Help button frame (row 5)
+        self.h_buttons_frame = Frame(self.converter_frame)
+        self.h_buttons_frame.grid(row=5, pady=10)
+
             #Calculation history button
-        self.history_button = Button(self.converter_frame, text="Calculation History", 
+        self.history_button = Button(self.h_buttons_frame, text="Calculation History", 
                                   padx=10, pady=10)
-        self.history_button.grid(row=5, column=0)
+        self.history_button.grid(row=0, column=0)
 
             #Help button
-        self.help_button = Button(self.converter_frame, text="Help", 
+        self.help_button = Button(self.h_buttons_frame, text="Help", 
                                   padx=10, pady=10)
-        self.help_button.grid(row=5, column=1)
+        self.help_button.grid(row=0, column=1)
 
 # main routine
 if __name__ == "__main__":
